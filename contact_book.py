55afd77f17ae350ae7ac48532a52662b3c7433f6
ECE-Indrajit <indrajitbhowmick1588@gmail.com>
2025-04-08 14:56:33 +0530

    Modifie code for contact book'
    cls

--- contact_book.py
+++ contact_book.py
@@ -1,13 +1,13 @@
 def display_menu():
-    print("\nContact Book Menu:")
+    print("Enter your choice:")
     print("1. Add a new contact")
     print("2. View all the contacts")
     print("3. Search for a contact")
     print("4. Delete a contact")
-    print("5. Exit")
+    print("5. Exit and deleat all contacts")
 
 def add_contact(contact_book):
-    name = input("Enter contact name: ")
+    name = input("\nEnter contact name: ")
     phone = input("Enter contact phone number: ")
     if name in contact_book:
         print("Contact already exists!")
@@ -15,7 +15,7 @@ def add_contact(contact_book):
         print("Phone number already exists!")
     else:
         contact_book[name] = phone
-        print("Contact ",name," added successfully to your Contact Book!")
+        print("Contact ",name," added successfully to your Contact Book!\n")
 
 def view_contacts(contact_book):
     if contact_book:
@@ -26,14 +26,14 @@ def view_contacts(contact_book):
         print("\nNo contacts found!")
 
 def search_contact(contact_book):
-    name = input("Enter the name to search: ")
+    name = input("\nEnter the name to search: ")
     if name in contact_book:
         print("Contact Found !\nName: ",name," Phone: ",contact_book[name])
     else:
         print("Contact not found in your Contact Book!")
 
 def delete_contact(contact_book):
-    name = input("Enter the name to delete: ")
+    name = input("\nEnter the name to delete: ")
     if name in contact_book:
         del contact_book[name]
         print("Contact ",name,"is deleted successfully from your Contact Book!")
@@ -41,8 +41,12 @@ def delete_contact(contact_book):
         print("Contact not found in your Contact Book!")
 
 def main():
+    print("\nWelcome to your Contact book\n")
+    print("This contact book allows you to add, view, search, and delete contacts.")
+    print("After you exit, all contacts will be deleted.")
     contact_book = {}
     while True:
+        print("--------------------------------------------------------")
         display_menu()
         choice = input("\nEnter your choice (1-5): ")
         if choice == "1":
@@ -57,7 +61,7 @@ def main():
             print("Exiting the contact book.\nAll Contacts are Deleated from your Contact Book!")
             break
         else:
-            print("Invalid choice! Please try again.")
+            print("\nError! : Please enter a valid choice.\n")
 
 if __name__ == "__main__":
     main()
