243026697c3e3f6eb1705c24d21023b523be5364
ECE-Indrajit <indrajitbhowmick1588@gmail.com>
2025-04-08 16:23:30 +0530

    Modified code for calculator

--- calculator.py
+++ calculator.py
@@ -1,33 +1,50 @@
-print("\n\nWelcome to the simple calculator\n")
-choice = "c"
-while choice == "c":
-    print("Here you have to first enter the operator and then two numbers to perform the operation\n")
+def display_menu():
+    print("--------------------------------------------------------")
     print("1. Enter + for addition")
     print("2. Enter - for subtraction")
     print("3. Enter * for multiplication")
     print("4. Enter / for division")
     print("5. Enter % for modulus division")
     print("6. Enter ^ or ** for power")
-    operator = input("\nEnter the operator to perform the operation :  ")
-    num1 = input("\nEnter first number: ")
-    num2 = input("Enter second number: ")
-    if (operator == "+"):
-        print("Result is = ",int(num1) + int(num2))
-    elif (operator == "-"):
-        print("Result is = ",int(num1) - int(num2))
-    elif (operator == "*"):
-        print("Result is = ",int(num1) * int(num2))
-    elif (operator == "/"):
-        if (int(num2) != 0):
-            print("Result is = ",int(num1) / int(num2))
+    print("7. Enter E or e to exit the calculator")
+
+def perform_operation(operator, num1, num2):
+    if operator == "+":
+        print("Result is =",num1 + num2)
+    elif operator == "-":
+        print("Result is =",num1 - num2)
+    elif operator == "*":
+        print("Result is =",num1 * num2)
+    elif operator == "/":
+        if num2 != 0:
+            print("Result is =",num1 / num2)
         else:
-            print("Error: Division by zero is not allowed")
-    elif (operator == "%"):
-        print("Result is = ",int(num1) % int(num2))
-    elif (operator == "^" or operator == "**"):
-        print("Result is = ",int(num1) ** int(num2))
+            return "Error: Division by zero is not allowed"
+    elif operator == "%":
+        print("Result is =",num1 % num2)
+    elif operator == "^" or operator == "**":
+        print("Result is =",num1 ** num2)
     else:
-        print("\nError : Please enter a valid operator\n")
-    choice = input("\nEnter c to continue or any other key to quit\n")
-    print("--------------------------------------------------------")
-print("Thank you for using the calculator")
+        print("Error! Please enter a valid operator") 
+
+def main():
+    print("\n\nWelcome to the simple calculator\n")
+    print("Here you have to first enter the operator and then two numbers to perform the operation\n")
+
+    while True:
+        display_menu()
+        operator = input("\nEnter the operator to perform the operation: ")
+        if operator == "E" or operator == "e":
+            print("Exiting the calculator. Thank you for using the simple calculator!")
+            break
+        num1 = input("\nEnter first number: ")
+        num2 = input("Enter second number: ")
+        if num1.isdigit() and num2.isdigit():
+            num1 = int(num1)
+            num2 = int(num2)
+            perform_operation(operator, num1, num2)
+        else:
+            print("\nInvalid input. Please enter valid numbers.\n")
+
+if __name__ == "__main__":
+    main()
